465dbf026114c81612b6b2ae0a2f122c0fec25b8
wnstjd9701 <wnstjd9701@naver.com>
2021-06-16 21:14:00 +0900

    Movie App

--- movie_app_2021/src/App.js
+++ movie_app_2021/src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
+const friends = ["dal", "mark", "lynn", "japan"]
+friends.map(current => {
+  console.log(current);
+  return 0;
+})
+friends.map(function(friend){
+  return friend + "👍";
+})
+function Movie({ name, picture, rating }){
+  return <div>
+    <h2>I like {name}</h2>
+    <h3>Rating: {rating} / 5.0 </h3>
+    <img src={picture} alt={name} />
+  </div>;
+}
+
+
+const movieList = [{
+  id:1,
+  name: 'Avengers',
+  image:"image1",
+  rating: 5
+},
+{
+  id:2,
+  name: "CaptainMarvel",
+  image: "image2"
+ , rating: 4.5
+}]
+ 
+function renderMovie(movie){
+  return <Movie key={movie.id} name={movie.name} picture={movie.image} rating={movie.rating} />
+}
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div> 
+      {movieList.map(renderMovie)}
     </div>
   );
 }
