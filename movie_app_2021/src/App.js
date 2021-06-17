6aa73c516578d827aa1b4ff5e28ef8cb5cc6e60d
wnstjd9701 <wnstjd9701@naver.com>
2021-06-17 18:09:28 +0900

    Movie App

--- movie_app_2021/src/App.js
+++ movie_app_2021/src/App.js
@@ -1,44 +1,27 @@
 import React from "react";
 
-const friends = ["dal", "mark", "lynn", "japan"]
-friends.map(current => {
-  console.log(current);
-  return 0;
-})
-friends.map(function(friend){
-  return friend + "👍";
-})
-function Movie({ name, picture, rating }){
-  return <div>
-    <h2>I like {name}</h2>
-    <h3>Rating: {rating} / 5.0 </h3>
-    <img src={picture} alt={name} />
-  </div>;
-}
-
-
-const movieList = [{
-  id:1,
-  name: 'Avengers',
-  image:"image1",
-  rating: 5
-},
-{
-  id:2,
-  name: "CaptainMarvel",
-  image: "image2"
- , rating: 4.5
-}]
- 
-function renderMovie(movie){
-  return <Movie key={movie.id} name={movie.name} picture={movie.image} rating={movie.rating} />
-}
-function App() {
-  return (
-    <div> 
-      {movieList.map(renderMovie)}
+class App extends React.Component{
+  state = {
+    count: 0    
+  
+  };
+  // using setState : To call render function
+  add = () => {
+    this.setState(current => ({count: current.count + 1}));
+    console.log(this.state.count);
+  };
+  minus =() => {
+    this.setState(current => ({count: current.count - 1}));
+    console.log(this.state.count);
+  };
+  render(){
+    return (
+    <div>
+      <h1>The number is : {this.state.count} </h1>
+      <button onClick={this.add}>Add</button>
+      <button onClick={this.minus}>Minus</button>
     </div>
-  );
+    );
+  }
 }
-
 export default App;
