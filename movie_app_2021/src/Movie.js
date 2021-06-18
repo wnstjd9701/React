a3625b1d83212f8c94095b5e14c7e09267a9f73b
wnstjd9701 <wnstjd9701@naver.com>
2021-06-18 17:35:37 +0900

    CSS

--- movie_app_2021/src/Movie.js
+++ movie_app_2021/src/Movie.js
@@ -1,7 +1,23 @@
 import React from "react";
+import "./App.css";
 
-function Movie({id, year, title, summary,poster}){
-    return <h4>{title}</h4>
+function Movie({ year, title, summary, poster, genres }) {
+    return (
+        <div className="movies">
+            <img src={poster} alt={title} title={title} />
+            <div className="movie_data">
+                <h3 className="movie_title">{title}</h3>
+                <h5 className="movie_year">{year}</h5>
+                <ul className="movie_genres">
+                    {genres.map((genre, index) => (
+                    <li key={index} className="genres_genre">{genre}</li>
+                    ))}
+                </ul>
+                
+                <p className="movie_summary">{summary}</p>              
+            </div>
+        </div>
+    );
 }
 
 export default Movie;
