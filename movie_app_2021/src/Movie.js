fb7bdf524b9817929abc1c8f834f93b15ca3aa91
wnstjd9701 <wnstjd9701@naver.com>
2021-06-20 15:35:22 +0900

    2021-06-20

--- movie_app_2021/src/Movie.js
+++ movie_app_2021/src/Movie.js
@@ -13,7 +13,6 @@ function Movie({ year, title, summary, poster, genres }) {
                     <li key={index} className="genres_genre">{genre}</li>
                     ))}
                 </ul>
-                
                 <p className="movie_summary">{summary}</p>              
             </div>
         </div>
