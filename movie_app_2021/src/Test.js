bb390b3fc2c2ebe4ebf1a4ec1a3a6690457e7135
wnstjd9701 <wnstjd9701@naver.com>
2021-06-21 14:28:17 +0900

    2021-06-21

--- movie_app_2021/src/Test.js
+++ movie_app_2021/src/Test.js
@@ -3,11 +3,14 @@ import React from "react";
 class Test extends React.Component{
     state = {
         count : 0
+        arr: 0
     }
     getCount = () => {
         this.setState(current => ({count: current.count + 1}));
     }
-    
+    getTest = () => {
+        this.setState(current => ( { arr: current.arr + 2}))
+    }
     render(){
         return (
             <button id="test" onClick = {this.getCount} >How many do you click? {this.state.count}</button>
